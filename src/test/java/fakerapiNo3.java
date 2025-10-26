d8377f41abb2490f3e315b11e0bdcf0e8667ea19
dzaky <dzakyabdi17@gmail.com>
2025-10-26 14:13:52 +0700

    test

--- src/test/java/fakerapiNo3.java
+++ src/test/java/fakerapiNo3.java
@@ -20,6 +20,7 @@ public class fakerapiNo3 {
 
         for(CompanyDetailPojo companyDetail: getCompaniesResponse.getCompany()){
             System.out.println(companyDetail.getId());
+
             // answer for question No. 3a
             Assert.assertNotNull(companyDetail.getId(), "Company ID is null");
         }
